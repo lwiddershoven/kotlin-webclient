53cb3809f5d4abb237563fcbc9b7417178063b87
Leon <qaz@dds.nl>
2020-10-09 10:19:50 +0200

    Added spring-fox, and a test resource to check whether the/swagger-ui endpoint also works out of the box with the Kotlin setup.

--- src/main/kotlin/net/leonw/kotlinwebclient/KotlinWebclientApplication.kt
+++ src/main/kotlin/net/leonw/kotlinwebclient/KotlinWebclientApplication.kt
@@ -2,10 +2,20 @@ package net.leonw.kotlinwebclient
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class KotlinWebclientApplication
 
 fun main(args: Array<String>) {
 	runApplication<KotlinWebclientApplication>(*args)
+}
+
+@RestController
+class TestResource {
+	@GetMapping("/bla")
+	fun bla() : String {
+		return "bla"
+	}
 }
